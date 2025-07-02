8612c5564d9dc5e724bed00fbfda583437d21218
agapebondservant <agapebondservant@yahoo.com>
2025-07-02 04:29:39 +0000

    updates

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,7 +11,7 @@ infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-    url = "https://testnet.infura.io/v3/af83c96cc0ff485bb901f9ed92726df3"  # FILL THIS IN
+    url = "https://mainnet.infura.io/v3/af83c96cc0ff485bb901f9ed92726df3"  # FILL THIS IN
     w3 = Web3(HTTPProvider(url))
     assert w3.is_connected(), f"Failed to connect to provider at {url}"
     return w3
