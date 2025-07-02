e411d7db5cd202fda23088840c46fecd8e9b0908
agapebondservant <agapebondservant@yahoo.com>
2025-07-02 07:09:17 +0000

    updates

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -28,7 +28,7 @@ def connect_with_middleware(contract_json):
     # TODO complete this method
 
     # The first section will be the same as "connect_to_eth()" but with a BNB url
-    url = "https://bsc-testnet.infura.io/v3/af83c96cc0ff485bb901f9ed92726df3"
+    url = "https://bsc-mainnet.infura.io/v3/af83c96cc0ff485bb901f9ed92726df3"
     w3 = Web3(HTTPProvider(url))
 
     # The second section requires you to inject middleware into your w3 object and
